3faed36a112f8ecbdeff3cb0c3ef451c2b848512
loai <loaiatabani@gmail.com>
2022-07-11 01:59:13 +0200

    dimentions

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -2,7 +2,7 @@
 
 import 'package:get/get.dart';
 
-//making the app responsive by calculating dimensions
+//making the app responsive by calculating dimensions for each device
 
 class Dimensions {
   static double screenHeight = Get.context!.height;
